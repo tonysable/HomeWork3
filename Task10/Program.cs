83305f554972c40db40b0c276e48993f1ebb6e9f
Sable <bandidos_150@mail.ru>
2022-10-08 23:16:37 +0300

    Задача 10

---
+++ Task10/Program.cs
@@ -0,0 +1,20 @@
+﻿string str;
+long a;
+Console.WriteLine("Введите число: ");
+str = Console.ReadLine();
+a = Convert.ToInt32(str);
+long c;
+c = a;
+long b;
+b = 0;
+    while (a > 0){
+        b *= 10;
+        b += a % 10;
+        a /= 10;
+     }
+if(c == b){
+    Console.WriteLine("Это палиндром!");
+}
+else{
+    Console.WriteLine("Не палиндром");
+}    
