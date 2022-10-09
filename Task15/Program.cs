10058adf622ff3e512b54f2763f830b2cda578fd
Sable <bandidos_150@mail.ru>
2022-10-09 12:16:26 +0300

    задача 15

---
+++ Task15/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while(count <= a){
+    Console.WriteLine($"{count} -> {count * count * count}");
+    count++;
+}
